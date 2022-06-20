72ab429af8fdeae82ffa938d36221b5966d77fcc
Patrick-FRE <patrick.lin@antra.com>
2022-06-20 15:54:22 -0400

    Update hoc part 2

--- React/react-app/src/components/BuyStock/BuyStockClass.js
+++ React/react-app/src/components/BuyStock/BuyStockClass.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import { withCounter } from '../../hoc/withCounter';
+import { withLoading } from '../../hoc/withLoading';
+import { getInitStockInfo } from '../../apis/stock.api'
 class BuyStockClass extends React.Component {
     render() {
         return (
             <section>
                 <h1>How many stock you want to buy</h1>
-                <button onClick={this.props.handleAdd}>
-                    +
-                </button>
-                <span>{this.props.counter}</span>
-                <button onClick={this.props.handleSub}>
-                    -
-                </button>
+                {
+                    this.props.isLoading ? this.props.showLoading('spinner') :
+                        <>
+                            <button onClick={this.props.handleAdd}>
+                                +
+                            </button>
+                            <span>{this.props.counter}</span>
+                            <button onClick={this.props.handleSub}>
+                                -
+                            </button>
+                        </>
+                }
             </section>
         );
     }
+
+    componentDidMount() {
+        this.props.startLoading()
+        getInitStockInfo().then(option => {
+            this.props.handleSetOption(option);
+            this.props.endLoading()
+        })
+    }
 }
 
-const BuyStockClassContainer = withCounter(BuyStockClass, { initCounter: 3, incrementNum: 3, decrementNum: 3 });
+//const BuyStockClassContainer = withNotification(withGlobal(withAuth(withCounter(withLoading(BuyStockClass)))));
+
+
+const BuyStockClassContainer = withCounter(withLoading(BuyStockClass));
 
 export default BuyStockClassContainer;
