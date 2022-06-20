3b9c774a5b09eaea2549893f3e2a475066bffed8
Patrick-FRE <patrick.lin@antra.com>
2022-06-20 14:43:02 -0400

    update hoc

--- React/react-app/src/App.js
+++ React/react-app/src/App.js
@@ -1,12 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 import CounterClass from './components/Counter/CounterClass'
-import CounterFn from './components/Counter/CounterFn'
+import CounterFn from './components/Counter/CounterFn';
+import BuyStockClass from './components/BuyStock/BuyStockClass'
+import { useState } from 'react';
+
+const pageInfo = [
+  {
+    pageName: "CounterClass",
+    pageComponent: CounterClass
+  },
+  {
+    pageName: "CounterFn",
+    pageComponent: CounterFn
+  },
+  {
+    pageName: "BuyStockClass",
+    pageComponent: BuyStockClass
+  }
+]
+
 function App() {
+
+  const [currentPage, setCurrentPage] = useState(pageInfo[1])
+
+  const hanldeChangePage = (activePageInfo) => {
+    setCurrentPage(activePageInfo)
+  }
+
+  const renderCurrentPage = () => {
+    const CurPage = currentPage.pageComponent
+    return <CurPage />
+  }
+
   return (
     <div className="App" hello="hello">
-      <CounterClass />
-      <CounterFn />
+      <header className="App__header">
+        <nav className="App__header__nav">
+          {pageInfo.map(page => {
+            return <a onClick={() => hanldeChangePage(page)} >{page.pageName}</a>
+          })}
+        </nav>
+      </header>
+      {
+        renderCurrentPage()
+      }
     </div>
   );
 }
--- React/react-app/src/components/Counter/CounterClass.js
+++ React/react-app/src/components/Counter/CounterClass.js
@@ -1,63 +1,53 @@
 import React from 'react';
+import { withCounter } from "../../hoc/withCounter"
 
 class Counter extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            counter: 0,
             title: "CounterClass"
         }
-        this.handleAdd = this.handleAdd.bind(this);
-        this.handleSub = this.handleSub.bind(this);
         this.hanldeAlert = this.hanldeAlert.bind(this);
     }
 
-    handleAdd() {
-        this.setState({
-            counter: this.state.counter + 1
-        })
-        this.setState({
-            counter: this.state.counter + 1
-        })
-    }
-    handleSub() {
-        this.setState({
-            counter: this.state.counter - 1
-        })
-    }
+
 
     hanldeAlert() {
         setTimeout(() => {
-            alert(this.state.counter)
+            alert(this.props.counter)
         }, 5000)
     }
 
     render() {
         console.log('render class compoennt')
+        console.log(this.props)
         return <section>
-            <header>{this.state.title}:{this.state.counter}</header>
-            <button onClick={this.handleAdd} >+</button><button onClick={this.handleSub}>-</button>
+            <header>{this.state.title}:{this.props.counter}</header>
+            <button onClick={this.props.handleAdd} >+</button><button onClick={this.props.handleSub}>-</button>
             <button onClick={this.hanldeAlert}>Alert after 5 s</button>
         </section>;
     }
 
 }
 
-export default Counter;
+
+const CounterContainer = withCounter(Counter);
+
+export default CounterContainer;
 
 
-let state = {
-    counter: 0
-}
+// let state = {
+//     counter: 0
+// }
 
-function setState(newState) {
-    state = { ...state, ...newState }
-}
+// function setState(newState) {
+//     state = { ...state, ...newState }
+// }
 
 
-this.setState({
-    counter: this.state.counter + 1
-})
-this.setState({
-    counter: this.state.counter + 1
-})
+// this.setState({
+//     counter: this.state.counter + 1
+// })
+// this.setState({
+//     counter: this.state.counter + 1
+// })
